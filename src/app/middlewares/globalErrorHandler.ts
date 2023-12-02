ae39a5489415df0561d41fb05001195353ed2f78
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2023-12-02 23:45:13 +0600

    added the expressErrorHandling and handle the error more structure way

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+const globalErrorHandler:ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
   const statusCode =err.statusCode|| 500;
   const message = err.message || 'Something went wrong';
