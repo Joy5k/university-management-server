fd1e853f2bded26d4448dc475407b0bc2b3e6bf1
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2023-12-04 03:33:38 +0600

    added the server error handler

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { AcademicSemesterRoute } from './app/modules/academicSemester/academicSemester.route';
-import express, { Application, NextFunction, Request, Response } from 'express';
-const app: Application = express();
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/students/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import express, { Application, Request, Response } from 'express';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
+const app: Application = express();
+
+//parsers
 app.use(express.json());
 app.use(cors());
 
-// app.use('/api/v1/students', StudentRoutes);
-// app.use('/api/v1/users', UserRoutes); 
-// app.use('/api/v1/academic-semesters', AcademicSemesterRoute); 
-app.use("/api/v1",router )
+// application routes
+app.use('/api/v1', router);
 
-app.get('/', (req: Request, res: Response) => {
+const test = async (req: Request, res: Response) => {
   const a = 10;
-
   res.send(a);
-});
+};
+
+app.get('/', test);
+
 app.use(globalErrorHandler);
 
+//Not Found
 app.use(notFound);
+
 export default app;
--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -7,9 +7,9 @@ const router = express.Router();
 
 router.post(
   '/create-academic-department',
-  validateRequest(
-    AcademicDepartmentValidation.CreateAcademicDepartmentValidationSchema,
-  ),
+  // validateRequest(
+  //   AcademicDepartmentValidation.CreateAcademicDepartmentValidationSchema,
+  // ),
   AcademicDepartmentController.createAcademicDepartment,
 );
 
